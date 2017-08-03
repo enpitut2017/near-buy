5b4627a570afcc3a6df61f0cb5f356b14f93ced8
urushiyama <aswif10flis1ntkb@gmail.com>
2017-08-03 16:31:34 +0900

    緯度軽度検索のルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'items#index'
-  post 'items/:id/sorted_shops', to: 'items#sorted_shops'
+  patch 'items/:id/sorted_shops', to: 'items#sorted_shops'
   resources :items, :only => [:index, :show]
 end
