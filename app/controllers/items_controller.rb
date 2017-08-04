7bc8098999eeae7ba3430c6bfe70c1339759a97d
TAKAHASHI Shuuji <shuuji3@gmail.com>
2017-08-04 12:34:24 +0900

    店舗を経緯度でソートして表示するアクションを実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,17 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def sorted_shops
+    lat = params[:lat]
+    lng = params[:lng]
+    @id = params[:id]
+    @shops = Shop.distance(lat, lng)
+  end
+
+  private
+
+  def shop_params
+    params.require(:shop).permit(:lat, :lng)
+  end
 end
