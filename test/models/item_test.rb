54b307e76c0e9b87f895d439f7b6539750735693
urushiyama <aswif10flis1ntkb@gmail.com>
2017-08-04 10:08:51 +0900

    商品詳細のバリデーションに対するテストを追加

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -11,4 +11,22 @@ class ItemTest < ActiveSupport::TestCase
     refute item.valid?, 'user is valid without a name'
     assert_not_nil item.errors[:name], 'no validation error for name present'
   end
+  
+  test 'invalid without price' do
+    item = Item.new(name: 'foo', price: nil)
+    refute item.valid?, 'item price is invalid without a price'
+    assert_not_nil item.errors[:price], 'no validation error for price present'
+  end
+  
+  test 'invalid negative price' do
+    item = Item.new(name: 'foo', price: -1)
+    refute item.valid?, 'item price is invalid negative value'
+    assert_not_nil item.errors[:price], 'no validation error for price present'
+  end
+  
+  test 'invalid non integer price' do
+    item = Item.new(name: 'foo', price: 0.1)
+    refute item.valid?, 'item price is invalid non integer value'
+    assert_not_nil item.errors[:price], 'no validation error for price present'
+  end
 end
