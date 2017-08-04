f1cd91766bf6d4319e08ec07c2c7d4231914c7b1
urushiyama <aswif10flis1ntkb@gmail.com>
2017-08-04 15:37:45 +0900

    [WIP] 在庫更新のアクションを追加
    find_byがなぜかRecordNotFoundを投げてくるので一旦放置。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # get 'stocks/:item_id/:shop_id/edit', to: 'register_stocks#edit'
-  # patch 'stocks/:item_id/:shop_id', to: 'register_stocks#update'
+  get 'stocks/:item_id/:shop_id/edit', to: 'register_stocks#edit'
+  patch 'stocks/:item_id/:shop_id', to: 'register_stocks#update'
 
   root 'items#index'
   patch 'items/:id/sorted_shops', to: 'items#sorted_shops'
