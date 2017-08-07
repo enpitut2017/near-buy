9f7f20ec2cb3f60c03141a4c23c1bf7e1c958798
TAKAHASHI Shuuji <shuuji3@gmail.com>
2017-08-07 15:14:46 +0900

    経緯度検索ができない問題を修正、在庫情報を○△×で表示するように修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,7 +11,7 @@ class ItemsController < ApplicationController
     lat = params[:lat]
     lng = params[:lng]
     @id = params[:id]
-    @item = Item.find_by(id: params[:id])
+    @item = Item.find(@id)
     if @item.shops.distance(lat, lng).present?
       @shops = @item.shops.distance(lat, lng)
     else
